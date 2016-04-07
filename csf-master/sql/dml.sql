aa14eea7eafb733e5fe8d08e6363297da227e9d1
CSTUDIO-Yamamoto <yamamoto@cstudio.jp>
2016-04-07 16:02:54 +0900

    テストデータSQL更新

--- csf-master/sql/dml.sql
+++ csf-master/sql/dml.sql
@@ -1,7 +1,7 @@
-insert into fw_user values('csf-test-app', 'CSFテストユーザ', '管理者', 'jp', 'ja', '2016-01-01 12:00:00', '2016-01-01 12:00:00', null);
+insert into fw_user (id, name, role, country, language, last_login_date)  values('csf-test-app', 'CSFテストユーザ', '管理者', 'jp', 'ja', null);
 -- password
-insert into fw_user_passwd values('csf-test-app', '$2a$10$1x/sImsfWiVTJqUbrkbSa.PNTxuwSlTjeAZ.n3/HN651xj20.9DFG', '2016-01-01 12:00:00', '2016-01-01 12:00:00');
+insert into fw_user_passwd (id, passwd) values('csf-test-app', '$2a$10$1x/sImsfWiVTJqUbrkbSa.PNTxuwSlTjeAZ.n3/HN651xj20.9DFG');
 
-insert into fw_user values('csf-test-app2', 'CSFテストユーザ2', '一般ユーザー', 'us', 'en', '2016-01-01 12:00:00', '2016-01-01 12:00:00', null);
+insert into fw_user (id, name, role, country, language, last_login_date)  values('csf-test-app2', 'CSFテストユーザ2', '一般ユーザー(英語)', 'us', 'en', null);
 -- password2
-insert into fw_user_passwd values('csf-test-app2', '$2a$10$hJotr3E9rBs9zJ77bti4pOQuFDLmxb7oe6.dd/5ITVAU8TrniEjOO', '2016-01-01 12:00:00', '2016-01-01 12:00:00');
+insert into fw_user_passwd (id, passwd) values('csf-test-app2', '$2a$10$hJotr3E9rBs9zJ77bti4pOQuFDLmxb7oe6.dd/5ITVAU8TrniEjOO');
