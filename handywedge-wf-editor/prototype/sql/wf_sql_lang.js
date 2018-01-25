6ee1ee144532d803e2e1aaa8f3dcdb9229105013
Kyosuke Miyatake <znsz4c@gmail.com>
2018-01-25 16:17:21 +0900

    [#154425410]文言修正

--- handywedge-wf-editor/prototype/sql/wf_sql_lang.js
+++ handywedge-wf-editor/prototype/sql/wf_sql_lang.js
@@ -6,19 +6,19 @@ function lang_change(lang){
     //上部バー
     $('#headbar .head_btn.add_box').text('新規ステータス');
     $('#headbar .head_btn.add_link').text('新規リンク');
-    $('#headbar .head_btn.tosql').text('SQL書き出し');
+    $('#headbar .head_btn.tosql').text('SQLダウンロード');
     $('#headbar .head_btn.load_json').text('読込');
     $('#headbar .head_btn.save_json').text('保存');
     //下部バー
     $('#belowbar .pull .status').text('ステータス');
-    $('#belowbar .pull .action').text('リンク');
+    $('#belowbar .pull .action').text('アクション');
     //テーブルのタイトル
     $('#view_status .contents_title').text('ステータス一覧');
     $('#view_action .contents_title').text('アクション一覧');
     //テーブル内部
     //status
     $('#view_status .table th.status').text('ステータスコード');
-    $('#view_status .table th.status_name').text('状態');
+    $('#view_status .table th.status_name').text('ステータス');
 
     $('#view_action .table th.action_code').text('アクションコード');
     $('#view_action .table th.action').text('アクション');
