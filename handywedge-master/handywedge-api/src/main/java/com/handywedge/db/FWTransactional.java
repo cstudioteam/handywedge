aff634fed13faa61058b2fb1f72d890e81837b8d
CSTUDIO-Yamamoto <yamamoto@cstudio.jp>
2020-06-12 12:34:12 +0900

    デフォルトDS名をFWに揃える

--- handywedge-master/handywedge-api/src/main/java/com/handywedge/db/FWTransactional.java
+++ handywedge-master/handywedge-api/src/main/java/com/handywedge/db/FWTransactional.java
@@ -23,7 +23,7 @@ import javax.interceptor.InterceptorBinding;
 /**
  * DBへアクセスするトランザクション開始点となるメソッドに宣言します。<br>
  * 当アノテーションを宣言しないとコネクションが生成されないため、参照のみの場合でも宣言が必要になります。<br>
- * 
+ *
  * @see javax.transaction.Transactional
  */
 public @interface FWTransactional {
@@ -32,7 +32,7 @@ public @interface FWTransactional {
   /**
    * トランザクションタイプを指定します。<br>
    * 現在のバージョンではREQUIREDのみサポートされます。
-   * 
+   *
    * @return トランザクションタイプ
    */
   public FWTransactional.FWTxType value() default FWTxType.REQUIRED;
@@ -45,7 +45,7 @@ public @interface FWTransactional {
   /**
    * トランザクション内で例外が発生した場合はインターセプターによりロールバックされますが、ここで指定した例外クラスがスローされた場合はロールバックを行いません。<br>
    * JTA仕様ではチェック例外が発生した場合はロールバックされませんが、handywedgeでは指定しないかぎりは<b>全ての例外</b>がロールバックされるので注意して下さい。<br>
-   * 
+   *
    * @return ロールバックさせない例外クラス配列
    */
   public Class<? extends Exception>[] dontRollbackOn() default {};
@@ -54,10 +54,10 @@ public @interface FWTransactional {
   /**
    * DB接続に使用するデータソースを指定します。<br>
    * web.xmlに指定したres-ref-nameを指定して下さい<br>
-   * 
+   *
    * @return ルックアップするデータソース名
    */
-  public String dataSourceName() default "jdbc/ds";
+  public String dataSourceName() default "jdbc/fw";
 
   /**
    * トランザクションタイプを定義します。
