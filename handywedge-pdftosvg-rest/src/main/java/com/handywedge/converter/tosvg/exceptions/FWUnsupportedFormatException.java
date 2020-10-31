3e071c85475b0e0345cf426994983a37f8244d99
cj li <li@cstudio.jp>
2020-10-31 16:40:34 +0900

    ごみ掃除

--- handywedge-pdftosvg-rest/src/main/java/com/handywedge/converter/tosvg/exceptions/FWUnsupportedFormatException.java
+++
@@ -1,9 +0,0 @@
-package com.handywedge.converter.tosvg.exceptions;
-
-public class FWUnsupportedFormatException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public FWUnsupportedFormatException(String msg){
-		super(msg);
-	}
-}
--- handywedge-pdftosvg-rest/src/main/java/com/handywedge/converter/tosvg/utils/FWConstantCode.java
+++
@@ -1,8 +0,0 @@
-package com.handywedge.converter.tosvg.utils;
-
-public class FWConstantCode {
-		public final static String PDF_TO_SVG_UNREAD = "変換元のファイルが読み込めません。[{0}]";
-		public final static String PDF_TO_SVG_UNSUPPORTED = "サポートしていないファイル形式です。";
-		public final static String PDF_TO_SVG_FAIL = "変換に失敗しました。";
-		public final static String PDF_TO_SVG_TIMEOUT = "変換処理でタイムアウトが発生しました。";
-}
