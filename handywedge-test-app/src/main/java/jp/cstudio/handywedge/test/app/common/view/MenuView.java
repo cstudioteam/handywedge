3e85a9a2515a5a179106d22df97ae15bb84970b7
山本剛 <yamamoto@cstudio.jp>
2022-02-17 17:26:36 +0900

    テストアプリのee9対応1

--- handywedge-test-app/src/main/java/jp/cstudio/handywedge/test/app/common/view/MenuView.java
+++ handywedge-test-app/src/main/java/jp/cstudio/handywedge/test/app/common/view/MenuView.java
@@ -5,11 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.faces.view.ViewScoped;
-import jakarta.inject.Inject;
-import jakarta.inject.Named;
-
 import org.primefaces.model.menu.DefaultMenuItem;
 import org.primefaces.model.menu.DefaultMenuModel;
 import org.primefaces.model.menu.DefaultSubMenu;
@@ -18,6 +13,10 @@ import org.primefaces.model.menu.MenuModel;
 import com.handywedge.context.FWContext;
 import com.handywedge.log.FWLogger;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
 import lombok.Getter;
 
 @ViewScoped
@@ -45,75 +44,49 @@ public class MenuView implements Serializable {
     model = new DefaultMenuModel();
 
     // ログは実験menu
-    DefaultSubMenu menu;
-    DefaultMenuItem item;
-//    if (role == Role.ADMIN) {
-//      menu = new DefaultSubMenu("ログ（管理者メニュー）");
-//      item = new DefaultMenuItem("アプリケーションログ");
-//      item.setIcon("fa fa-file-text");
-//      item.setOutcome("/contents/log/app_log.xhtml");
-//      menu.addElement(item);
-//
-//      item = new DefaultMenuItem("サーバログ");
-//      item.setIcon("fa fa-file-text");
-//      menu.addElement(item);
-//
-//      item = new DefaultMenuItem("エラーログ");
-//      item.setIcon("fa fa-ban");
-//      item.setOutcome("/contents/log/error_log.xhtml");
-//      menu.addElement(item);
-//      model.addElement(menu);
-//    }
+    DefaultSubMenu menu1 = DefaultSubMenu.builder().label("データベース").build();
+    DefaultMenuItem item1 = DefaultMenuItem.builder().value("マスタ参照")
+        .outcome("/contents/db/master.xhtml").icon("fa fa-database").build();
+    menu1.getElements().add(item1);
+    model.getElements().add(menu1);
 
-    menu = new DefaultSubMenu("データベース");
-    item = new DefaultMenuItem("マスタ参照");
-    item.setOutcome("/contents/db/master.xhtml");
-    item.setIcon("fa fa-database");
-    menu.addElement(item);
-    model.addElement(menu);
+    DefaultSubMenu menu2 = DefaultSubMenu.builder().label("プロパティ").build();
+    DefaultMenuItem item2 = DefaultMenuItem.builder().value("プロパティ一覧")
+        .outcome("/contents/config/resources.xhtml").icon("fa fa-wrench").build();
+    menu2.getElements().add(item2);
+    model.getElements().add(menu2);
 
-    menu = new DefaultSubMenu("プロパティ");
-    item = new DefaultMenuItem("プロパティ一覧");
-    item.setOutcome("/contents/config/resources.xhtml");
-    item.setIcon("fa fa-wrench");
-    menu.addElement(item);
-    model.addElement(menu);
+    DefaultSubMenu menu3 = DefaultSubMenu.builder().label("PDF").build();
+    DefaultMenuItem item3 = DefaultMenuItem.builder().value("請求書サンプル")
+        .outcome("/contents/report/pdf/seikyusho.xhtml").icon("fa fa-file-pdf-o").build();
+    menu3.getElements().add(item3);
+    model.getElements().add(menu3);
 
-    menu = new DefaultSubMenu("PDF");
-    item = new DefaultMenuItem("請求書サンプル");
-    item.setOutcome("/contents/report/pdf/seikyusho.xhtml");
-    item.setIcon("fa fa-file-pdf-o");
-    menu.addElement(item);
-    model.addElement(menu);
+    DefaultSubMenu menu4 = DefaultSubMenu.builder().label("暗号化").build();
+    DefaultMenuItem item4 = DefaultMenuItem.builder().value("文字列")
+        .outcome("/contents/crypto/crypto.xhtml").icon("fa fa-key").build();
+    menu4.getElements().add(item4);
+    model.getElements().add(menu4);
 
-    menu = new DefaultSubMenu("暗号化");
-    item = new DefaultMenuItem("文字列");
-    item.setOutcome("/contents/crypto/crypto.xhtml");
-    item.setIcon("fa fa-key");
-    menu.addElement(item);
-    model.addElement(menu);
+    DefaultSubMenu menu5 = DefaultSubMenu.builder().label("メール").build();
+    DefaultMenuItem item5 = DefaultMenuItem.builder().value("メール送信")
+        .outcome("/contents/mail/mail.xhtml").icon("fa fa-envelope-o").build();
+    menu5.getElements().add(item5);
+    model.getElements().add(menu5);
 
-    menu = new DefaultSubMenu("メール");
-    item = new DefaultMenuItem("メール送信");
-    item.setOutcome("/contents/mail/mail.xhtml");
-    item.setIcon("fa fa-envelope-o");
-    menu.addElement(item);
-    model.addElement(menu);
 
-    menu = new DefaultSubMenu("ワークフロー");
-    item = new DefaultMenuItem("ワークフローサンプル");
-    item.setOutcome("/contents/workflow/workflowView.xhtml");
-    item.setIcon("fa fa-users");
-    menu.addElement(item);
-    item = new DefaultMenuItem("ワークフローサンプル2");
-    item.setOutcome("/contents/workflow2/workflow2View.xhtml");
-    item.setIcon("fa fa-users");
-    menu.addElement(item);
-    model.addElement(menu);
+    DefaultSubMenu menu6 = DefaultSubMenu.builder().label("ワークフロー").build();
+    DefaultMenuItem item6 = DefaultMenuItem.builder().value("ワークフローサンプル")
+        .outcome("/contents/workflow/workflowView.xhtml").icon("fa fa-users").build();
+    menu6.getElements().add(item6);
+    DefaultMenuItem item7 = DefaultMenuItem.builder().value("ワークフローサンプル2")
+        .outcome("/contents/workflow2/workflow2View.xhtml").icon("fa fa-users").build();
+    menu6.getElements().add(item7);
+    model.getElements().add(menu6);
 
     // 実験menu
-//    new Thread(new AppLogReader(1000)).start();
-//    new Thread(new ErrorLogReader(5000)).start();
+    // new Thread(new AppLogReader(1000)).start();
+    // new Thread(new ErrorLogReader(5000)).start();
     logger.perfEnd("init", startTime);
   }
 
